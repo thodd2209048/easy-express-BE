97cbd83f040b5bc2a92d1dba209770dcf1db9683
thodd2209048 <114908027+thodd2209048@users.noreply.github.com>
2023-12-17 23:39:12 +0700

    active WebConfig

--- src/main/java/com/example/easyexpressbackend/controller/StaffController.java
+++ src/main/java/com/example/easyexpressbackend/controller/StaffController.java
@@ -14,7 +14,6 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/staffs")
-@CrossOrigin(origins = "http://localhost:3000")
 public class StaffController {
     private final StaffService service;
 
--- src/main/java/com/example/easyexpressbackend/controller/TrackingController.java
+++ src/main/java/com/example/easyexpressbackend/controller/TrackingController.java
@@ -10,7 +10,6 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(path = "/api/trackings")
-@CrossOrigin(origins = "http://localhost:3000")
 public class TrackingController {
     private final TrackingService service;
 
