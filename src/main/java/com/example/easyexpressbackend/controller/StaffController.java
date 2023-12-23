013c257f76198646500d4e2fa8d5df2abba0c09d
thodd2209048 <114908027+thodd2209048@users.noreply.github.com>
2023-12-23 23:53:22 +0700

    filter, sort listStaff

--- src/main/java/com/example/easyexpressbackend/controller/StaffController.java
+++ src/main/java/com/example/easyexpressbackend/controller/StaffController.java
@@ -7,6 +7,7 @@ import com.example.easyexpressbackend.service.StaffService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
@@ -22,14 +23,26 @@ public class StaffController {
         this.service = service;
     }
 
-    @GetMapping({"/",""})
-    public Page<StaffResponse> listStaffs(@PageableDefault(sort = {"id"}, direction = Sort.Direction.DESC) Pageable pageable,
-            @RequestParam (required = false) Long hubId){
+    @GetMapping({"/", ""})
+    public Page<StaffResponse> listStaffs(
+            @PageableDefault(sort = {"id"}) Pageable pageable,
+            @RequestParam(required = false) Long hubId,
+            @RequestParam(value = "sort-field", required = false) String sortField,
+            @RequestParam(required = false, defaultValue = "asc") String direction) {
+
+        Sort.Direction sortDirection = direction.equals("desc") ?
+                Sort.Direction.DESC : Sort.Direction.ASC;
+
+        Sort sort = sortField.equals("name") ?
+                Sort.by(sortDirection,"name") : Sort.by(sortDirection,"id");
+
+        pageable = PageRequest.of(pageable.getPageNumber(), pageable.getPageSize(), sort);
+
         return service.listStaffs(pageable, hubId);
     }
 
-    @PostMapping({"/",""})
-    public StaffResponse addStaff(@RequestBody @Valid AddStaffDto addStaffDto){
+    @PostMapping({"/", ""})
+    public StaffResponse addStaff(@RequestBody @Valid AddStaffDto addStaffDto) {
         return service.addStaff(addStaffDto);
     }
 
@@ -37,12 +50,12 @@ public class StaffController {
     public StaffResponse updateStaff(
             @PathVariable Long id,
             @RequestBody UpdateStaffDto updateStaffDto
-    ){
+    ) {
         return service.updateStaff(id, updateStaffDto);
     }
 
     @DeleteMapping("{id}")
-    public void deleteStaff(@PathVariable Long id){
+    public void deleteStaff(@PathVariable Long id) {
         service.deleteStaff(id);
     }
 }
